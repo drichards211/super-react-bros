d715273dee55efd01b90901974aee6797476fa67
Daniel <drichards211@yahoo.com>
2020-08-30 21:36:13 -0400

    Add logic handling

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 /* import sprites from './logo.svg'; */
 import './App.css';
 
 function RenderBrother() {
-  return (
-    <div className="Render-brother super-mario">
-
-    </div>
+  return ( 
+    <div>
+      <MarioStateManager/>
+      <TestButton/>
+    </div> 
   )
 }
 
+const TestButton = () => {
+  return (  
+    <button onClick={() => console.log("mushroom")}>
+      Mushroom
+    </button>
+  )
+}
 
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
+const MarioStateManager = ({ button }) => {
+  console.log("MarioStateManager() ran")
+  
+  const [marioState, setMarioState] = useState({
+    brother: "luigi",
+    super: true,
+    fire: false,
+    invincible: false,
+    invinciTimer: 0,
+    alive: true,
+    coins: 0,
+    timer: 90,
+  });
+  
+  // MARIO STATE LOGIC:
+  switch (true) {
+    case marioState.alive && marioState.invincible: // Mario-Luigi is INVINCIBLE:
+      return (marioState.super) ?
+      ( <div className="Render-brother invincible-super"></div> ) : // Invincible Super
+      ( <div className="Render-brother invincible"></div> ) // Invincible Regular
+    case marioState.alive && marioState.brother === "mario": // Mario is ALIVE:
+      if (marioState.fire) { // Fire Mario
+        return ( <div className="Render-brother fire"></div> )
+      } else { 
+        return (marioState.super) ? 
+          ( <div className="Render-brother mario-super"></div> ) : // Super Mario
+          ( <div className="Render-brother mario"></div> ) // Mario
+      }
+    case marioState.alive && marioState.brother === "luigi" : // Luigi is ALIVE:
+      if (marioState.fire) { // Fire Luigi
+        return ( <div className="Render-brother fire"></div> )
+      } else { 
+        return (marioState.super) ? 
+          ( <div className="Render-brother luigi-super"></div> ) : // Super Luigi
+          ( <div className="Render-brother luigi"></div> ) // Luigi
+      }
+    default: // He's DEAD, Jim!
+      switch (true) {
+        case marioState.fire && marioState.timer === 0: // Timer ran out while Mario/Luigi was Fire:
+          return ( <div className="Render-brother fire-dead"></div> );
+        case marioState.brother === "luigi": // Luigi is DEAD:
+          return ( <div className="Render-brother luigi-dead"></div> );
+        default: // Mario is DEAD:
+          return ( <div className="Render-brother mario-dead"></div> );
+      }
+  }
+}
 
 export default RenderBrother;
