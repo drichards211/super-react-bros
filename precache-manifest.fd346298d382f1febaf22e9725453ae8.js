5d4c66bce819fd9fd9580fbc12bf23612404f35b
Daniel <drichards211@yahoo.com>
2021-05-27 18:43:30 -0400

    Updates

---
+++ precache-manifest.fd346298d382f1febaf22e9725453ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4408a587af6acfc75dca51d3dedeb0",
+    "url": "/super-react-bros/index.html"
+  },
+  {
+    "revision": "0e79101679922c289d76",
+    "url": "/super-react-bros/static/css/main.915b6747.chunk.css"
+  },
+  {
+    "revision": "4225caa12a5c98cf66be",
+    "url": "/super-react-bros/static/js/2.3a7fbfb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/super-react-bros/static/js/2.3a7fbfb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e79101679922c289d76",
+    "url": "/super-react-bros/static/js/main.4aca585a.chunk.js"
+  },
+  {
+    "revision": "33242981f46d3d8151b8",
+    "url": "/super-react-bros/static/js/runtime-main.005ea6bc.js"
+  },
+  {
+    "revision": "74496d9086d97aaeeafb3085e9957668",
+    "url": "/super-react-bros/static/media/PressStart2P-vaV7.74496d90.ttf"
+  }
+]);
